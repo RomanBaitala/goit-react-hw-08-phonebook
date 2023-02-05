12cff1d1b711b200ce3830c6005c2f2a80d055e3
RomanBaitala <63609124+RomanBaitala@users.noreply.github.com>
2023-02-05 00:49:03 +0000

    Deploying to gh-pages from @ RomanBaitala/goit-react-hw-08-phonebook@547a102a5ebf8708474a6ad40f544276758c255d 🚀

---
+++ static/js/983.e4f6c96e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{3983:function(e,o,n){n.r(o),n.d(o,{HomePage:function(){return c}});var r=n(2916),t=n(184),c=function(){return(0,t.jsx)(r.ch,{children:(0,t.jsx)(r.wh,{children:"Welcome to your phonebook manager"})})};o.default=c}}]);
+//# sourceMappingURL=983.e4f6c96e.chunk.js.map
